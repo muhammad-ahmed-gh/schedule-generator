cf13d3156f45e053513e8090765470e65950d8a1
Muhammad-Ahmed-GH <Muhammad.Ahmed.Connect@gmail.com>
2025-11-09 23:26:58 +0200

    chore: adding variables to css file and adding title to task name and description (js)

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,12 @@
 let menuBtn = document.querySelector("header .bars");
 menuBtn.onclick = () => menuBtn.parentElement.classList.toggle("open");
+
+let taskNames = document.querySelectorAll(".task .task-name");
+for(let taskName of taskNames) {
+  taskName.title = taskName.textContent;
+}
+
+let taskDescs = document.querySelectorAll(".task .task-desc");
+for(let taskDesc of taskDescs) {
+  taskDesc.title = taskDesc.textContent;
+}
